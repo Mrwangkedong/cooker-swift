cf91a7e6e6c1712bd6b2532f96f41050a3a1931b
tony <1241925557@qqcom>
2020-06-26 05:22:19 +0800

    Initial Commit

---
+++ TTT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TTT
+//
+//  Created by Admin on 2020/6/26.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TTTTests/TTTTests.swift
@@ -0,0 +1,34 @@
+//
+//  TTTTests.swift
+//  TTTTests
+//
+//  Created by Admin on 2020/6/26.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import TTT
+
+class TTTTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
